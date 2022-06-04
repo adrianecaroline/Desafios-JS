9784ee5ea022ffcd7aced9b8dc5ab5918d59f9f6
Driel <97626768+adrianecaroline@users.noreply.github.com>
2022-06-04 16:25:46 -0300

    Update Desafio1.js

--- Desafio1.js
+++ Desafio1.js
@@ -1,9 +1,3 @@
-/* DESAFIO 1 - Faça uma função que escreva no console uma palavra de acordo com a especificação abaixo:
-
-- Utiliza os valores de 1 a 299.
-- Para cada valor, se ele for múltiplo de 3, deverá exibir a palavra: VAMO.
-- Para cada valor, se ele for múltiplo de 5, deverá exibir a palavra: BORA.
-- Para cada valor, se ele for múltiplo de 3 e 5 ao mesmo tempo, deverá exibir a palavra: VAMBORA.*/
 
 function lerValores() {
     for (var num1 = 1; num1 <= 299; num1++) {

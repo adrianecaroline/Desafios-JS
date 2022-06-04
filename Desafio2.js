f0043d5cbca67aa62c31742eb9b2c5b06b62dc02
Driel <97626768+adrianecaroline@users.noreply.github.com>
2022-06-04 16:26:07 -0300

    Update Desafio2.js

--- Desafio2.js
+++ Desafio2.js
@@ -1,8 +1,3 @@
-/* Desafio 2 - Faça uma função que recebe um valor inteiro e positivo:
-
-- Se o valor for menor que 350, a função deverá incrementar o valor recebido com o valor 5.
-- Se o novo valor for múltiplo de 5, deverá exibir a mensagem: "MÚLTIPLO DE 5".
-- Se o novo valor for múltiplo de 7, deverá exibir a mensagem: "MÚLTIPLO DE 7".*/
 
 function recebeValor (){
     for(var valor = 1; valor < 350; valor+=5) {

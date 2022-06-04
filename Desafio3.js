e7b65064759fc159eb8189a2f203c6463b90269e
Driel <97626768+adrianecaroline@users.noreply.github.com>
2022-06-04 16:25:34 -0300

    Update Desafio3.js

--- Desafio3.js
+++ Desafio3.js
@@ -1,14 +1,3 @@
-/* DESAFIO 3- Faça uma função que exibe a sequência abaixo usando as especificações:
-
-Exibir os 100 primeiros números da sequência da acima.
-- O primeiro valor é 0.
-- O segundo valor é 1.
-- O terceiro valor em diante é o resultado da soma de valores, por exemplo:
-1.Se somar o primeiro valor e o segundo, o resultado é 1.
-2.Se somar o segundo valor e o resultado da soma anterior,  o novo resultado será 2, e assim por diante.
-*/
-
-
 
 function fibonacci (){
     
